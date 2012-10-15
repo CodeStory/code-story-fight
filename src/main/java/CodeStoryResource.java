25f64c3fc04c27bbf1eb8d9f1686cad8df0360c0
David Gageot <david@gageot.net>
2012-10-15 12:52:22 +0200

    Headers

--- src/main/java/CodeStoryResource.java
+++ src/main/java/CodeStoryResource.java
@@ -9,8 +9,6 @@ import org.lesscss.LessException;
 import twitter4j.TwitterException;
 
 import javax.activation.MimetypesFileTypeMap;
-import javax.ws.rs.*;
-import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -18,6 +16,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
 
 import java.io.File;
@@ -25,12 +24,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
 
 @Path("/")
 @Singleton
 public class CodeStoryResource {
-  private Planning planning;
-  private Users users;
+  private final Planning planning;
+  private final Users users;
 
   @Inject
   public CodeStoryResource(Planning planning, Users users) throws IOException {
@@ -84,18 +84,32 @@ public class CodeStoryResource {
 
   @GET
   @Path("{path : .*\\.less}.css")
-  public File style(@PathParam("path") String path) throws IOException, LessException {
+  public synchronized Response style(@PathParam("path") String path) throws IOException, LessException {
     File output = new File("target", path + ".css");
     new LessCompiler().compile(file(path), output, false);
-    return output;
+    return staticResource(output);
   }
 
   @GET
   @Path("{path : .*}")
   public Response staticResource(@PathParam("path") String path) throws IOException {
-    File file = file(path);
+    return staticResource(file(path));
+  }
+
+  static Response staticResource(File file) throws IOException {
     String mimeType = new MimetypesFileTypeMap().getContentType(file);
-    return Response.ok(file, mimeType).build();
+    return Response.ok(file, mimeType).cacheControl(buildCacheControl()).lastModified(new Date()).build();
+  }
+
+  static CacheControl buildCacheControl() {
+    CacheControl cacheControl = new CacheControl();
+
+    if (!"dev".equals(System.getProperty("env"))) {
+      cacheControl.setMaxAge(3600); // 1 hour
+      cacheControl.setNoTransform(false); // bug is jax-rs
+    }
+
+    return cacheControl;
   }
 
   static File file(String path) throws IOException {
