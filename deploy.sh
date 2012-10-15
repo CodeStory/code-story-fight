f7fab15e3a0d5390fdee49186d2ded0800cc7ef1
David Gageot <david@gageot.net>
2012-10-15 10:49:39 +0200

    Use foreman to start the server

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,13 @@ function exit_ok {
 	alert_user "${1}"; exit 0
 }
 
+echo '--- CHECK FOR PROCFILE ---'
+foreman check
+if [ $? -ne 0 ]; then
+	exit_ko "No procfile to be run with foreman"
+fi
+
+
 echo '--- BUILD ---'
 mvn -B -e clean install -Dmaven.test.skip=true
 if [ $? -ne 0 ]; then
@@ -26,13 +33,12 @@ echo '--- CONFIGURE ---'
 export PORT=8080
 
 echo '--- STOP ---'
-killall java
+killall "foreman: master"
 
 echo '--- START ---'
-nohup java -Xmx512M -cp target/classes:target/dependency/* CodeStoryServer < /dev/null > prod.log 2>&1 &
+nohup foreman start < /dev/null > prod.log 2>&1 &
 if [ $? -ne 0 ]; then
 	exit_ko "Unable to start"
 fi
 
-exit_ok "Devoxx fight started!"
-
+exit_ok "Devoxx fight started on port #{PORT}"
