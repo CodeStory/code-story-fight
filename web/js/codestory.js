9a4267399a0f4cdce071e9e4ece2eb4dada87261
David Gageot <david@gageot.net>
2012-10-24 21:00:56 +0200

    Improve JS and style

--- web/js/codestory.js
+++ web/js/codestory.js
@@ -1,15 +1,4 @@
 var current_login = 'dgageot';
-
-function refreshRegistrationLinks() {
-  $.getJSON('registrations/' + current_login, function (json) {
-    $('.register').html('Register');
-
-    _.each(json, function (talkId) {
-      $('#session-' + talkId + ' a.register').html('Unregister');
-    });
-  });
-}
-
 var dayLabels = {
   '2012-11-12':'Monday, Nov. 12 2012',
   '2012-11-13':'Tuesday, Nov. 13 2012',
@@ -18,29 +7,44 @@ var dayLabels = {
   '2012-11-16':'Friday, Nov. 16 2012'
 }
 
-function refreshPlanning() {
-  $.getJSON('planning.json', function (json) {
-    _.each(json.days, function (day) {
-      day.dayLabel = dayLabels[day.day];
+function set_content(selector, template, data) {
+  $(selector).html(Hogan.compile($(template).html()).render(data));
+}
 
-      _.each(day.slots, function (slot) {
-        _.each(slot.talks, function (talk) {
-          talk.speakers_string = talk.speakers.join(', ');
-        });
-      });
+function initAuthenticationState() {
+  set_content('#nav', '#header-template', {authenticated:false});
+}
+
+function refreshRegistrationLinks() {
+  $.getJSON('registrations/' + current_login, function (json) {
+    $('.register').html('Register');
+    $('.talk').removeClass('registered');
+
+    _.each(json, function (talkId) {
+      $('#session-' + talkId + ' .register').html('Unregister');
+      $('#session-' + talkId).addClass('registered');
     });
-
-    $("#content").html(Hogan.compile($('#talks-template').html()).render(json));
-    refreshRegistrationLinks();
   });
 }
 
-function register(login, talkId, callback) {
-  $.post('register', {login:login, talkId:talkId}, callback);
+function enrichPlanning(planning) {
+  _.each(planning.days, function (day) {
+    day.dayLabel = dayLabels[day.day];
+
+    _.each(day.slots, function (slot) {
+      _.each(slot.talks, function (talk) {
+        talk.speakers_string = talk.speakers.join(', ');
+      });
+    });
+  });
 }
 
-function unregister(login, talkId, callback) {
-  $.post('unregister', {login:login, talkId:talkId}, callback);
+function refreshPlanning() {
+  $.getJSON('planning.json', function (json) {
+    enrichPlanning(json);
+    set_content('#content', '#talks-template', json);
+    refreshRegistrationLinks();
+  });
 }
 
 function listenRegistrationClicks() {
@@ -48,17 +52,13 @@ function listenRegistrationClicks() {
     var talkId = $(this).attr('data-talk');
 
     if ($(this).html() == "Register") {
-      register(current_login, talkId, refreshRegistrationLinks);
+      $.post('register', {login:current_login, talkId:talkId}, refreshRegistrationLinks);
     } else {
-      unregister(current_login, talkId, refreshRegistrationLinks);
+      $.post('unregister', {login:current_login, talkId:talkId}, refreshRegistrationLinks);
     }
 
     return false;
   });
-}
-
-function initAuthenticationState() {
-  $('header').append(Hogan.compile($('#header-template').html()).render({authenticated:false}));
 }
 
 function listenSearch() {
@@ -83,11 +83,10 @@ function listenSearch() {
 
     $('.day_wrapper:not(:has(.talk:visible))').hide();
     $('.slot:not(:has(.talk:visible))').hide();
-
     $('.hour a:hidden').each(function () {
       $('.toc-link[data-toc="' + this.id + '"]').hide();
-    });
-  });
+    })
+  })
 }
 
 $(document).ready(function () {
@@ -95,4 +94,4 @@ $(document).ready(function () {
   refreshPlanning();
   listenRegistrationClicks();
   listenSearch();
-});
+})
