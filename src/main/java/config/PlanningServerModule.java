9cfa64c86d47817192435b4fc27f000f18d04029
David Gageot <david@gageot.net>
2012-10-25 10:15:11 +0200

    Shorter code

--- src/main/java/config/PlanningServerModule.java
+++ src/main/java/config/PlanningServerModule.java
@@ -6,37 +6,34 @@ import com.google.common.base.Strings;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.name.Named;
-import com.google.inject.name.Names;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class PlanningServerModule extends AbstractModule {
-  private static final String OAUTH_CONSUMER_KEY = "OAUTH_CONSUMER-KEY";
-  private static final String OAUTH_CONSUMER_SECRET = "OAUTH_CONSUMER-SECRET";
-  private static final String OAUTH_CALLBACK = "OAUTH_CALLBACK";
+import static com.google.inject.name.Names.named;
 
+public class PlanningServerModule extends AbstractModule {
   @Override
   protected void configure() {
-    bindConstant().annotatedWith(Names.named("oAuth.callback")).to(env(OAUTH_CALLBACK));
-    bindConstant().annotatedWith(Names.named("oAuth.consumerKey")).to(env(OAUTH_CONSUMER_KEY));
-    bindConstant().annotatedWith(Names.named("oAuth.consumerSecret")).to(env(OAUTH_CONSUMER_SECRET));
+    bindConstant().annotatedWith(named("oAuth.callback")).to(env("OAUTH_CALLBACK"));
+    bindConstant().annotatedWith(named("oAuth.key")).to(env("OAUTH_CONSUMER-KEY"));
+    bindConstant().annotatedWith(named("oAuth.secret")).to(env("OAUTH_CONSUMER-SECRET"));
 
     bind(Authenticator.class).to(TwitterAuthenticator.class).asEagerSingleton();
   }
 
   @Provides
-  private Twitter createCodeStoryConfigurationForTwitter(@Named("oAuth.consumerKey") String oAuthConsumerKey, @Named("oAuth.consumerSecret") String oAuthConsumerSecret) {
-    Configuration config = new ConfigurationBuilder()
-        .setOAuthConsumerKey(oAuthConsumerKey)
-        .setOAuthConsumerSecret(oAuthConsumerSecret)
-        .build();
+  private Configuration createConfiguration(@Named("oAuth.key") String key, @Named("oAuth.secret") String secret) {
+    return new ConfigurationBuilder().setOAuthConsumerKey(key).setOAuthConsumerSecret(secret).build();
+  }
 
+  @Provides
+  private Twitter createTwitter(Configuration config) {
     return new TwitterFactory(config).getInstance();
   }
 
-  private String env(String name) {
+  private static String env(String name) {
     return Strings.nullToEmpty(System.getenv(name));
   }
 }
