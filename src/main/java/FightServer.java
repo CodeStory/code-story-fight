5b6f4b64ee211acece2e68ab947426bfe8e9ebd4
David Gageot <david@gageot.net>
2012-11-14 16:52:19 +0100

    Use mustache on the server side

--- src/main/java/FightServer.java
+++ src/main/java/FightServer.java
@@ -1,14 +1,16 @@
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.util.Map;
 
 public class FightServer implements HttpHandler {
 	final Scorer scorer;
@@ -23,17 +25,23 @@ public class FightServer implements HttpHandler {
 		URL votesUrl = Resources.getResource("starsPerTalk.json");
 
 		TalkIds talkIds = new TalkIds(planningUrl);
-		Votes scores = new Votes(votesUrl);
-		Scorer scorer = new Scorer(talkIds, scores);
+		Votes votes = new Votes(votesUrl);
+		Scorer scorer = new Scorer(talkIds, votes);
 
 		new FightServer(scorer).start(8080);
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		String body = Files.toString(new File("index.html"), Charsets.UTF_8);
+		DefaultMustacheFactory mustacheFactory = new DefaultMustacheFactory();
+		Mustache mustacheTemplate = mustacheFactory.compile("index.html");
+		StringWriter writer = new StringWriter();
+		Map<String, String> data = ImmutableMap.of(
+			"leftKeyword", "AngularJS");
+		mustacheTemplate.execute(writer, data);
 
-		byte[] response = body.getBytes();
+
+		byte[] response = writer.toString().getBytes();
 		exchange.sendResponseHeaders(200, response.length);
 		exchange.getResponseHeaders().add("content-type", "text/html");
 		exchange.getResponseBody().write(response);
