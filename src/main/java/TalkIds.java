918c116268df063ff7349432e43df1ead8f9fac8
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2012-11-14 15:41:44 +0100

    Refactoring

--- src/main/java/TalkIds.java
+++ src/main/java/TalkIds.java
@@ -1,13 +1,12 @@
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import com.google.common.io.Resources;
 import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,7 +17,7 @@ public class TalkIds {
 	private final Map<Integer, List<String>> keywordsByIds;
 
 	@VisibleForTesting
-	 TalkIds(Map<Integer, List<String>> keywordsByIds) {
+	TalkIds(Map<Integer, List<String>> keywordsByIds) {
 		this.keywordsByIds = keywordsByIds;
 	}
 
@@ -29,12 +28,12 @@ public class TalkIds {
 		for (Day day : days.days) {
 			for (Slot slot : day.slots) {
 				for (Talk talk : slot.talks) {
-					List<String> keywords = Lists.newArrayList();
-					keywords.add(talk.title);
-					keywords.add(talk.summary);
-					keywords.addAll(talk.tags);
-					keywords.addAll(talk.speakers);
-					keywordsByIds.put(talk.id, keywords);
+					keywordsByIds.put(talk.id, ImmutableList.<String>builder()
+							.add(talk.title)
+							.add(talk.summary)
+							.addAll(talk.tags)
+							.addAll(talk.speakers)
+							.build());
 				}
 			}
 		}
@@ -55,12 +54,15 @@ public class TalkIds {
 	static class Days {
 		List<Day> days;
 	}
+
 	static class Day {
 		List<Slot> slots;
 	}
+
 	static class Slot {
 		List<Talk> talks;
 	}
+
 	static class Talk {
 		int id;
 		String title;
