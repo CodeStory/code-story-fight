217d7967ee6396c0e28833197406cc2de1123252
David Gageot <david@gageot.net>
2012-11-14 15:50:51 +0100

    Parsing scores

---
+++ src/main/java/Scores.java
@@ -0,0 +1,35 @@
+import com.google.common.base.Splitter;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.base.Objects.firstNonNull;
+import static com.google.common.collect.Iterables.getFirst;
+import static com.google.common.collect.Iterables.getLast;
+import static com.google.common.collect.Maps.newHashMap;
+import static java.lang.Integer.valueOf;
+
+public class Scores {
+	private Map<Integer, Integer> talksIds;
+
+	public Scores(Map<Integer, Integer> talksIds) {
+		this.talksIds = talksIds;
+	}
+
+	public Scores(String talkIdsJson) {
+		this.talksIds = newHashMap();
+		Iterable<String> talkAndScores = Splitter.on(",").split(talkIdsJson.subSequence(1, talkIdsJson.length() - 1));
+		for (String talkAndScore : talkAndScores) {
+			Iterable<String> split = Splitter.on(':').trimResults().split(talkAndScore);
+			this.talksIds.put(valueOf(getFirst(split, "-1")), valueOf(getLast(split, "-1")));
+		}
+	}
+
+	public Integer getScore(List<Integer> talkIds) {
+		Integer sum = 0;
+		for (Integer talkId : talkIds) {
+			sum += firstNonNull(talksIds.get(talkId), 0);
+		}
+		return sum;
+	}
+}
---
+++ src/test/java/ScoresTest.java
@@ -0,0 +1,24 @@
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ScoresTest {
+
+	@Test
+	public void should_sum_up_scores_from_talk_ids() {
+		Scores scores = new Scores(ImmutableMap
+			.of(931, 19, 805, 18, 200, 3));
+		assertThat(scores.getScore(newArrayList(931, 805))).isEqualTo(37);
+	}
+
+	@Test
+	public void should_sum_up_scores_from_talk_ids_json() {
+		Scores scores = new Scores("{931: 19, 805: 18, 200: 3}");
+		assertThat(scores.getScore(newArrayList(931, 805))).isEqualTo(37);
+	}
+
+
+
+}
