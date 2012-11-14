648b9f6b4b84086b5ea300658bc02026eead9e0c
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2012-11-14 16:09:03 +0100

    Refactor http server

--- src/main/java/FightServer.java
+++ src/main/java/FightServer.java
@@ -10,7 +10,7 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URL;
 
-public class FightServer {
+public class FightServer implements HttpHandler {
 	final Scorer scorer;
 	HttpServer server;
 
@@ -25,20 +25,20 @@ public class FightServer {
 		new FightServer(new Scorer(new TalkIds(planningUrl), new Scores(votesUrl))).start(8080);
 	}
 
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		String body = Files.toString(new File("index.html"), Charsets.UTF_8);
+
+		byte[] response = body.getBytes();
+		exchange.sendResponseHeaders(200, response.length);
+		exchange.getResponseHeaders().add("content-type", "text/html");
+		exchange.getResponseBody().write(response);
+		exchange.close();
+	}
+
 	public void start(int port) throws IOException {
 		server = HttpServer.create(new InetSocketAddress(port), 0);
-		server.createContext("/", new HttpHandler() {
-			@Override
-			public void handle(HttpExchange exchange) throws IOException {
-				String body = Files.toString(new File("index.html"), Charsets.UTF_8);
-
-				byte[] response = body.getBytes();
-				exchange.sendResponseHeaders(200, response.length);
-				exchange.getResponseHeaders().add("content-type", "text/html");
-				exchange.getResponseBody().write(response);
-				exchange.close();
-			}
-		});
+		server.createContext("/", this);
 		server.start();
 	}
 
